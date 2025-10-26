be9e4f4f7bedca91bcca5250b881c435ba3cd597
ry0hei-kobayashi <kobayashi.ryohei621@mail.kyutech.jp>
2025-10-27 01:02:11 +0900

    update file path & add installer

--- blackscreen.py
+++ blackscreen.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import gi
-import subprocess
-
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
 class BlackCover(Gtk.Window):
-    def __init__(self, x, y, w, h):
+    def __init__(self, monitor):
         super().__init__()
         self.set_decorated(False)
         self.set_app_paintable(True)
@@ -14,44 +12,34 @@ class BlackCover(Gtk.Window):
         self.set_skip_pager_hint(True)
         self.set_accept_focus(False)
         self.set_keep_above(True)
-        self.set_type_hint(Gdk.WindowTypeHint.NORMAL)
-        self.move(x, y)
-        self.set_default_size(w, h)
-        self.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0, 0, 0, 1))
-        self.connect("button-press-event", self.on_click)
-        self.show_all()
+        self.set_type_hint(Gdk.WindowTypeHint.UTILITY)
+        self.stick()
         self.fullscreen()
-        try:
-            self.get_window().set_cursor(Gdk.Cursor.new(Gdk.CursorType.BLANK_CURSOR))
-        except:
-            pass
 
-    def on_click(self, widget, event):
-        Gtk.main_quit()
+        geo = monitor.get_geometry()
+        self.move(geo.x, geo.y)
+        self.set_default_size(geo.width, geo.height)
+        self.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0, 0, 0, 1))
 
-def get_monitors():
-    result = subprocess.run(['xrandr', '--listactivemonitors'], stdout=subprocess.PIPE, encoding='utf-8')
-    lines = result.stdout.strip().split('\n')[1:]  # skip header
-    monitors = []
-    for line in lines:
-        parts = line.split()
-        res_pos = parts[2]  # e.g., "3440/797x1440/333+0+0"
-        size_str, pos_str = res_pos.split('+', 1)
-        width_raw, height_raw = size_str.split('x')
-        width = int(width_raw.split('/')[0])
-        height = int(height_raw.split('/')[0])
-        x, y = map(int, pos_str.split('+'))
-        monitors.append((x, y, width, height))
-    return monitors
+        # Hide the cursor when the window is realized
+        self.connect("realize", self.hide_cursor)
+        self.connect("button-press-event", lambda *_: Gtk.main_quit())
+        self.connect("key-press-event", lambda w, e: Gtk.main_quit() if e.keyval == Gdk.KEY_Escape else None)
+        self.show_all()
+
+    def hide_cursor(self, widget):
+        gdk_window = self.get_window()
+        if gdk_window:
+            blank = Gdk.Cursor.new(Gdk.CursorType.BLANK_CURSOR)
+            gdk_window.set_cursor(blank)
 
 def main():
     Gtk.init([])
-    covers = []
-    for x, y, w, h in get_monitors():
-        cover = BlackCover(x, y, w, h)
-        covers.append(cover)
+    display = Gdk.Display.get_default()
+    for i in range(display.get_n_monitors()):
+        monitor = display.get_monitor(i)
+        BlackCover(monitor)
     Gtk.main()
 
 if __name__ == "__main__":
     main()
-
---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo ln -s $PWD/blackscreen.py /usr/local/bin/blackscreen
+sudo mkdir -p ~/.local/share/applications
+sudo cp $PWD/blackscreen.desktop ~/.local/share/applications/
+
+
